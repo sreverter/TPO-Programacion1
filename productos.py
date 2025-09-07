f6e827466ce1e4e434528cb066f5a605ea60f0d7
sreverter <santiago.reverter@insite-la.com>
2025-09-07 17:04:43 -0300

    Squashed commit of the following:
    commit 11a37655c996ed3fa5685b0054071d9714988019
    Author: sreverter <santiago.reverter@insite-la.com>
    Date:   Sun Sep 7 16:58:25 2025 -0300
    
        arreglado de funciones, comentado de la data que no se va a usar y unificacion de la funcion de agregar registros para que funcione con productos y proveedores
    
    commit c83a7394e22e74a5285edabbd92113a4c8def83f
    Author: Santiago Reverter <sreverter@uade.edu.ar>
    Date:   Mon Sep 1 20:26:10 2025 -0300
    
        modificacion de data con lo que nos dijo el profe
    
    commit d1621c50ea2fefa998a4db634f704e50b7c43f31
    Author: agusegimenez <agusegimenez@gmail.com>
    Date:   Sun Aug 31 20:45:53 2025 -0300
    
        agregar producto y buscar proveedor para verificar de que exista
    
    commit 1b79f0465e962b48b5b4934b6cd892f27d1e0998
    Author: agusegimenez <agusegimenez@gmail.com>
    Date:   Sun Aug 31 18:41:30 2025 -0300
    
        unifique las funciones de las entidades en el file de funciones y deje un submenu en cada entidad. agregue el menu principal (basico) al main que hace el llamado a cada submenu y agregue la entidad de usuarios para empezar a trabajar en el registro y login

--- productos.py
+++ productos.py
@@ -5,25 +5,19 @@ descripcion_columnas = ["ID", "ID_CATEGORIA", "NOMBRE", "ID_PROVEEDOR", "STOCK",
 productos = data.productos
 
 def menu_productos():
+
+    producto = 0
     while True:
         print("\n--- Gestión de Productos ---")
         opcion = input("1: Ver | 2: Buscar | 3: Agregar | 4: Desactivar | 5: Salir\nOpción: ")
 
         if opcion == "1":
             f.mostrar_tabla(productos, descripcion_columnas)
-            orden_por_precio = input("¿Desea ordenar por precio? (s/n): ").lower()
-            if orden_por_precio == "s":
-                ascendente = input("Quiere que sea de menor a mayor? (s/n)")
-                if ascendente == "s":
-                    productos.sort(key=lambda x: x[5])
-                else:
-                    productos.sort(key=lambda x: x[5], reverse=True) 
-                f.mostrar_tabla(productos, descripcion_columnas)
         elif opcion == "2":
             id_buscar = int(input("Ingrese ID: "))
             print(f.buscar_id(productos, id_buscar))
         elif opcion == "3":
-            f.agregar_registro(productos, descripcion_columnas)
+            f.agregar_registro(productos, descripcion_columnas, producto)
         elif opcion == "4":
             id_eliminar = int(input("Ingrese ID: "))
             f.desactivar_registro(productos, id_eliminar)
